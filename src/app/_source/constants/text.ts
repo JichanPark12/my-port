610000c21fc50796370814eae51aab09f1590733
pjc0179 <pjc0179@gmail.com>
2026-05-07 16:06:14 +0900

    fix:타입에러수정

--- src/app/_source/constants/text.ts
+++ src/app/_source/constants/text.ts
@@ -1,5 +1,5 @@
-import { TextObject } from "../types/text-object";
-import { ScreenComponents } from "../types/type";
+import { TextObject } from '../types/text-object';
+import { ScreenComponents } from '../types/type';
 
 export const screenTextObject: Record<
   ScreenComponents,
@@ -7,19 +7,19 @@ export const screenTextObject: Record<
 > = {
   home: {
     list: [
-      { id: "first", text: "     안녕하세요     ", className: "" },
-      { id: "second", text: " 논리적인 사고를 좋아하며 ", className: "" },
-      { id: "third", text: " 되네 가 아닌 왜? 를 생각하는 ", className: "" },
-      { id: "fourth", text: "프론트엔드 개발자 박지찬 입니다.", className: "" },
+      { id: 'first', text: '     안녕하세요     ', className: '' },
+      { id: 'second', text: ' 논리적인 사고를 좋아하며 ', className: '' },
+      { id: 'third', text: ' 되네 가 아닌 왜? 를 생각하는 ', className: '' },
+      { id: 'fourth', text: '프론트엔드 개발자 박지찬 입니다.', className: '' },
     ],
   },
   skills: {
-    list: [{ id: "first", text: "하이용" }],
+    list: [{ id: 'first', text: '하이용' }],
   },
-  myProject: {
-    list: [{ id: "first", text: "하이용" }],
+  myProjects: {
+    list: [{ id: 'first', text: '하이용' }],
   },
   aboutMe: {
-    list: [{ id: "", text: "" }],
+    list: [{ id: '', text: '' }],
   },
 };
